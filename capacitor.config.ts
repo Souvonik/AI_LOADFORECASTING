2bf24692032ab2b46e7378e61322a0c705c89cc0
Souvonik <souvonikbasu01@gmail.com>
2025-08-11 00:03:23 +0530

    apk commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.company.appname',
+  appName: 'AI Load Forecasting',
+  webDir: 'dist'
+};
+
+export default config;
